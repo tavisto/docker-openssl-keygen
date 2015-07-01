e0f18ce666c0a66b593640c7d01cfb3faa45fadd
Tavis Aitken <tavisto@tavisto.net>
2015-07-01 17:39:37 -0600

    Updated all the scripts to sign the certs for 5 years instead of just one.

--- bin/gen-ca.sh
+++ bin/gen-ca.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 echo 01 > keys/ca.srl
 openssl genrsa -out keys/ca-key.pem
-openssl req -subj '/CN=docker' -new -x509 -days 365 -key keys/ca-key.pem -out keys/ca.pem
+openssl req -subj '/CN=docker' -new -x509 -days 1825 -key keys/ca-key.pem -out keys/ca.pem
--- bin/gen-server.sh
+++ bin/gen-server.sh
@@ -6,6 +6,6 @@ echo subjectAltName = IP:$IP,IP:127.0.0.1 > keys/server-extfile.cnf
 cat keys/server-extfile.cnf
 openssl genrsa -out keys/server-key.pem 2048
 openssl req -subj "/CN=$HOSTNAME" -new -key keys/server-key.pem -out keys/server.csr
-openssl x509 -req -days 365 -in keys/server.csr -CA keys/ca.pem -CAkey keys/ca-key.pem -out keys/server-cert.pem -extfile keys/server-extfile.cnf
+openssl x509 -req -days 1825 -in keys/server.csr -CA keys/ca.pem -CAkey keys/ca-key.pem -out keys/server-cert.pem -extfile keys/server-extfile.cnf
 openssl x509 -text -in keys/server-cert.pem -noout
 openssl rsa -in keys/server-key.pem -out keys/server-key.pem
